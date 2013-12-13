61117e64b2c66b4e424271cd2de7dd08e4caf42e
luyanfei <luyanfei78@163.com>
2013-12-13 15:29:17 +0800

    add ImportUserWindow.

--- src/main/java/cn/jhc/myexam/vaadin/component/ImportUsersWindow.java
+++ src/main/java/cn/jhc/myexam/vaadin/component/ImportUsersWindow.java
@@ -1,39 +1,37 @@
-package cn.jhc.myexam.vaadin.view;
+package cn.jhc.myexam.vaadin.component;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Upload;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 @Component
 @Scope("prototype")
-public class ImportUsersView extends CustomComponent implements View {
+public class ImportUsersWindow extends Window {
 
 	private static final long serialVersionUID = 1L;
 	
 	private Upload upload;
 	
-	public ImportUsersView() {
+	public ImportUsersWindow() {
+		super("从Excel导入考生帐号");
+		center();
 		VerticalLayout layout = new VerticalLayout();
 		layout.setImmediate(false);
-		layout.setWidth("100%");
-		layout.setHeight("100%");
-		setWidth("100%");
-		setHeight("100%");
-		setCompositionRoot(layout);
+		layout.setSizeUndefined();
+		setContent(layout);
 		upload = new Upload();
 		upload.setButtonCaption("上传Excel文件");
 		layout.addComponent(upload);
 		layout.setComponentAlignment(upload, Alignment.MIDDLE_CENTER);
+		
+		setClosable(true);
+		setModal(true);
+		setResizable(false);
 	}
 
-	@Override
-	public void enter(ViewChangeEvent event) {
-	}
 
 }

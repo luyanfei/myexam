72fd68539a98b7e26e206ad3baaf506460c58bf6
luyanfei <luyanfei78@163.com>
2014-01-23 19:20:17 +0800

    EntityTableFactory added.

--- src/main/java/cn/jhc/myexam/vaadin/util/EntityTableFactory.java
+++ src/main/java/cn/jhc/myexam/vaadin/util/EntityTableFactory.java
@@ -1,18 +1,36 @@
 package cn.jhc.myexam.vaadin.util;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.Entity;
 
+import cn.jhc.myexam.annotation.Description;
+
 import com.vaadin.data.util.BeanContainer;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.Table.ColumnHeaderMode;
+import com.vaadin.ui.themes.BaseTheme;
 
 public class EntityTableFactory {
 	
+	public static interface DeleteColumnCallback<T> {
+		public void onDelete(T delItem);
+	}
+	
+	/**
+	 * 
+	 * @param clazz
+	 * 		entity class beneath the table.
+	 * @param collection
+	 * 		collection of entity objects.
+	 * @return
+	 * 		the constructed vaadin table.
+	 */
 	public static <T> Table getEntityTable(Class<T> clazz, Collection<T> collection) {
 		Entity entityAnnotation = clazz.getAnnotation(Entity.class);
 		if(entityAnnotation == null)
@@ -20,12 +38,44 @@ public class EntityTableFactory {
 		BeanContainer<Long, T> container = new BeanContainer<Long, T>(clazz);
 		container.setBeanIdProperty("id");
 		container.addAll(collection);
-		//TODO: inotrospector get all visible ids
-
+		List<String> propertyNameList = new ArrayList<String>();
+		List<String> descriptionList = new ArrayList<String>();
 		for(Field field : clazz.getFields()) {
-			
+			Description description = field.getAnnotation(Description.class);
+			if(description != null) {
+				propertyNameList.add(field.getName());
+				descriptionList.add(description.value());
+			}
 		}
-		
-		return null;
+		Table table = new Table();
+		table.setContainerDataSource(container, propertyNameList);
+		table.setColumnHeaderMode(ColumnHeaderMode.EXPLICIT);
+		table.setColumnHeaders(descriptionList.toArray(new String[0]));
+		return table;
+	}
+	
+	public static <T> Table getEntityTable(Class<T> clazz, Collection<T> collection, 
+			final DeleteColumnCallback<T> callback) {
+		Table table = getEntityTable(clazz, collection);
+		table.addGeneratedColumn("delete", new Table.ColumnGenerator() {
+			
+			@Override
+			public Object generateCell(final Table source, final Object itemId, Object columnId) {
+				Button removeButton = new Button("删除");
+				removeButton.setStyleName(BaseTheme.BUTTON_LINK);
+				removeButton.addClickListener(new Button.ClickListener() {
+					
+					@Override
+					public void buttonClick(ClickEvent event) {
+						BeanContainer<Long, T> bc = (BeanContainer<Long, T>)source.getContainerDataSource();
+						T delItem = bc.getItem(itemId).getBean(); 
+						source.removeItem(itemId);
+						callback.onDelete(delItem);
+					}
+				});
+				return removeButton;
+			}
+		});
+		return table;
 	}
 }

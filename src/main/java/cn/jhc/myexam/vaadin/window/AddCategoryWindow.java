8651d4598121618598ff0b8b087b6955f41ce714
luyanfei <luyanfei78@163.com>
2014-02-08 17:44:17 +0800

    addCustomField is work now.

--- src/main/java/cn/jhc/myexam/vaadin/window/AddCategoryWindow.java
+++ src/main/java/cn/jhc/myexam/vaadin/window/AddCategoryWindow.java
@@ -1,10 +1,13 @@
 package cn.jhc.myexam.vaadin.window;
 
+import java.util.Arrays;
+
 import cn.jhc.myexam.server.domain.Category;
 import cn.jhc.myexam.vaadin.builder.VaadinEntityBuilder;
 import cn.jhc.myexam.vaadin.builder.VaadinEntityBuilder.EntityFormCallback;
 
 import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.FormLayout;
 
 public class AddCategoryWindow extends FormWindow {
@@ -25,6 +28,9 @@ public class AddCategoryWindow extends FormWindow {
 					@Override
 					public void addCustomField(FormLayout formLayout,
 							FieldGroup fieldGroup) {
+						ComboBox box = new ComboBox("选择父类别",Arrays.asList("类别一","类别二"));
+						fieldGroup.bind(box, "parent");
+						formLayout.addComponent(box);
 					}
 				});
 		return formLayout;
